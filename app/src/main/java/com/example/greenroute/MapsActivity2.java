e7edb21d994e6325238969cb437a98ae32da6543
Dazasaur <mikkel.sahlholdt@gmail.com>
2020-03-23 12:45:02 +0100

    new ver

---
+++ app/src/main/java/com/example/greenroute/MapsActivity2.java
@@ -0,0 +1,144 @@
+package com.example.greenroute;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+import com.google.maps.model.TravelMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        String testTitle = "Air Quality: 9/10";
+
+        MarkerOptions routeStart = new MarkerOptions().position(new LatLng(57.012346,9.993241)).title("Start");
+        MarkerOptions routeDest = new MarkerOptions().position(new LatLng(57.022170,9.876142)).title("Destination");
+        MarkerOptions wayPoint = new MarkerOptions().position(new LatLng(57.029320,9.898425)).title(testTitle);
+        LatLng start = new LatLng(57.012346,9.993241);
+        mMap.addMarker(routeStart);
+        mMap.addMarker(routeDest);
+        mMap.addMarker(wayPoint);
+        String routeStartLatLng = "57.012346,9.993241";
+        String routeDestLatLng = "57.022170,9.876142";
+        String routeWayPoint = "57.029320,9.898425";
+        /*
+
+        mMap.addMarker(new MarkerOptions().position(routeStart.getPosition()).title("Start position"));
+
+        LatLng dest = new LatLng(57.022170,9.876142);
+        mMap.addMarker(new MarkerOptions().position(routeDest.getPosition()).title("Destination"));
+
+        LatLng waypoint = new LatLng(57.029320,9.898425);
+        mMap.addMarker(new MarkerOptions().position(waypoint).title("Waypoint"));
+        */
+
+        //Define list to get all latlng for the route
+        List<LatLng> path = new ArrayList();
+
+
+        //Execute Directions API request
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyAt_YF-0dcSocGGxznWNBx60sAfEbPUc2Q")
+                .build();
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, routeStartLatLng, routeDestLatLng).waypoints(routeWayPoint).mode(TravelMode.WALKING);
+        try {
+            DirectionsResult res = req.await();
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            Log.e("MAP", ex.getLocalizedMessage());
+        }
+
+        if (path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start, 6));
+
+
+    }
+}
